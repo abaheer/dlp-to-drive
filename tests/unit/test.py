b4beebd5fc076c242d9859111925c6ae35f135aa
bahm941 <baheer1234@live.com>
2024-02-19 18:48:31 +1300

    update tests

--- tests/unit/test.py
+++ tests/unit/test.py
@@ -3,14 +3,22 @@ import pytest
 from subprocess import run
 from convert import Convert
 
-def downloadPlaylist():
+def test_download_playlist():
     test = Convert("https://www.youtube.com/playlist?list=PLKgGttdYjfrC0yf7p2h18Dg-vq6MCWWDX", True)
 
     # downloads playlist and creates a new folder within the directory from current directory
-    run(f'yt-dlp -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" {test.link} -x')
+    run(f'yt-dlp -o "test_playlist/%(title)s.%(ext)s" {test.link} -x')
 
-def downloadSong():
-    test = Convert("https://youtu.be/oz4q-pMuve4?list=PLKgGttdYjfrC0yf7p2h18Dg-vq6MCWWDX", True)
+    assert os.path.isfile(os.getcwd() + '/test_playlist/GOOD GRIEF.webm')
+    assert os.path.isfile(os.getcwd() + '/test_playlist/feelsbad.webm')
+    assert os.path.isfile(os.getcwd() + '/test_playlist/2018.webm')
+
+def test_download_song():
+    test = Convert("https://youtu.be/oz4q-pMuve4?si=RGcd6aiD4_HC6lRg", True)
 
     #downloads single file or downloads playlist into current directory
-    run('yt-dlp "https://youtu.be/oz4q-pMuve4?list=PLKgGttdYjfrC0yf7p2h18Dg-vq6MCWWDX" -x')
+    run(f'yt-dlp -o /single_output.%(ext)s {test.link} -x')
+    assert os.path.isfile(os.getcwd() + '/single_output.webm')
+
+    #delete file after test
+    os.remove(os.getcwd() + '/single_output.webm')
